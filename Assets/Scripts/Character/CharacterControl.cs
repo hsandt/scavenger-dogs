7548d78aab595b358ba4113cfb96628904e184b7
Hyper Sonic <hs@fuwar.in>
2020-05-01 16:14:28 +0100

    [INPUT] Switch to Send Messages behavior + Player Input Manager
    so multiplayer input always works

--- Assets/Scripts/Character/CharacterControl.cs
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -2,60 +2,30 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-using CommonsHelper;
-
 public class CharacterControl : MonoBehaviour
 {
     /* Sibling components */
     private CharacterMaster characterMaster;
     
-    /* Owned */
-    private CharacterControls m_CharacterControls;
-    
     /* State */
     private Vector2 m_MoveIntention = Vector2.zero;
     public Vector2 MoveIntention => m_MoveIntention;
 
-    private void OnDestroy()
+    private void Start()
     {
-        m_CharacterControls.Dispose();
+        Setup();
     }
 
-    private void Awake()
+    private void Setup()
     {
-        characterMaster = this.GetComponentOrFail<CharacterMaster>();
-
-        m_CharacterControls = new CharacterControls();
-
-        if (characterMaster.playerId < Gamepad.all.Count)
-        {
-            Gamepad gamepad = Gamepad.all[characterMaster.playerId];
-            m_CharacterControls.devices = new[] {gamepad};
-        }
-        else
-        {
-            // Fix this later by handling InputSystem.onDeviceChange, PlayerInput.DeviceLostEvent, PlayerInput.DeviceRegainedEvent
-            m_CharacterControls.devices = new InputDevice[] {};
-            Debug.LogWarningFormat("Gamepad {0} not plugged during CharacterControl.Awake. Character {1} won't be controllable.",
-                characterMaster.playerId, 
-                characterMaster.color);
-        }
+        m_MoveIntention = Vector2.zero;
     }
 
-    public void OnEnable()
-    {
-        m_CharacterControls.Enable();
-    }
-
-    public void OnDisable()
-    {
-        m_CharacterControls.Disable();
-    }
-
-    void Update()    
+    // Input Action callback
+    private void OnMove(InputValue value)
     {
         // Just use the input value, as we set up Binary Cardinal Processor for gamepad stick input in Controls asset,
         // so that each coordinate is 0/1 as in old school games with D-pad, even when using the left stick to move.
-        m_MoveIntention = m_CharacterControls.Player.Move.ReadValue<Vector2>();
+        m_MoveIntention = value.Get<Vector2>();
     }
 }
