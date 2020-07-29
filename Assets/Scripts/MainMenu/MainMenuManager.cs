655245154d999009c6519d45d55cbdd890e37aaf
Hyper Sonic <hs@gamedesignshortcut.com>
2020-07-29 18:41:41 +0200

    [UI] Added Start button to Main Menu > Title Menu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using CommonsPattern;
+using UnityConstants;
+
+public class MainMenuManager : SingletonManager<MainMenuManager>
+{
+    /// Button event callback
+    public void StartStage1()
+    {
+        SceneManager.LoadScene(Scenes.Stage1);
+    }
+}
